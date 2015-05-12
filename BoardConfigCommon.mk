ae646ab4efa8a2121b514c1dbd8c132fd9c3503b
Adeel Javed <alwaysadeel@gmail.com>
2015-05-12 21:19:22 +0500

    cleanup to fix boot

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -70,16 +70,6 @@ TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := false
 TARGET_BOOTANIMATION_USE_RGB565 := true
 
-# Enable dex-preoptimization to speed up first boot sequence
-ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),userdebug)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
-DONT_DEXPREOPT_PREBUILTS := true
-
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
